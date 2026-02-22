fceaeec5566e97ae898d8382fac3c4e06ec05ffc
Monu <rockmonu45@gmail.com>
2026-02-22 19:10:27 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,19 @@
+// console.log("Hello i'm Monu and i am learning javascript");
+
+
+// alert("hello world");
+
+
+// // thiss is single line comments
+
+
+//  /* this is    multiple
+
+//     line 
+
+//      comments */
+
+
+
+//  prompt("enter your name: ");
+
